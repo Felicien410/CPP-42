3e5133510df8e52b45ba6b75d79b4e86708fb335
Felicien410 <97636902+Felicien410@users.noreply.github.com>
2023-11-24 13:57:18 +0100

    d

--- CPP_8/ex00/main.cpp
+++ CPP_8/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: feliciencatteau <feliciencatteau@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:19:14 by feliciencat       #+#    #+#             */
-/*   Updated: 2023/11/14 15:01:29 by feliciencat      ###   ########.fr       */
+/*   Updated: 2023/11/24 13:56:22 by feliciencat      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int main()
     tab.push_back(13);
     tab.push_back(25);
     tab.push_back(333);
-    easyFind(tab, 2);
+    std::cout << easyFind(tab, 2) << std::endl;
     std::cout << easyFind(tab, 333) << std::endl;
 }
